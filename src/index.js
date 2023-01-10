4fbfd2a1a1f16ee853fa9b7fbf6fb1861dbd9609
Vladimir Zinovik <zinovikv@gmail.com>
2023-01-10 22:16:00 +0300

    feat: finish reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let reverseNumber = 0;
+  let lastInteger;
+  let positiveNumber = Math.abs(n);
+
+  while (positiveNumber != 0) {
+    lastInteger = positiveNumber % 10;
+    reverseNumber = reverseNumber * 10 + lastInteger;
+    positiveNumber = Math.floor(positiveNumber / 10);
+  }
+  return reverseNumber;
 }
